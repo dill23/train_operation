3fb8322a29cdfe170ad7b21c0124c183e0ea784a
Dylan Hilse <dlhilse04@email.com>
2023-04-15 17:49:35 -0400

    start of the train info code

--- src/TrainOperations.py
+++ src/TrainOperations.py
@@ -1,3 +1,12 @@
+ 
 
 __version__ = "0.0.1"
 __author__ = "Dylan Hilse"
+
+
+cars = {}
+
+car_id = int(input("enter car's id please: "))
+cars[car_id] = { "id" : car_id}
+
+print(cars)
---
+++ src/train_info.py
@@ -0,0 +1,7 @@
+
+
+class TrainInfo():
+
+    def __init__(self):
+        self.cars = {}
+        
