17fedad783c6a895c0ca0bcc2b266bd2041f3c51
dill23 <66688804+dill23@users.noreply.github.com>
2023-05-10 21:15:54 +0000

    created addcar method

--- src/TrainOperations.py
+++ src/TrainOperations.py
@@ -2,11 +2,9 @@
 
 __version__ = "0.0.1"
 __author__ = "Dylan Hilse"
+import train_info as ti
 
+train = ti.TrainInfo()
 
-cars = {}
+train.addcar()
 
-car_id = int(input("enter car's id please: "))
-cars[car_id] = { "id" : car_id}
-
-print(cars)
--- src/train_info.py
+++ src/train_info.py
@@ -1,7 +1,21 @@
-
+import traceback
 
 class TrainInfo():
 
-    def __init__(self):
-        self.cars = {}
-        
+  def __init__(self):
+      self.cars = []
+
+  def addcar(self):
+    try:
+      cardic = {}
+      carid = int(input("please enter the cars id number: "))
+      cardic['carid'] = carid
+      cartype = input("enter the car type: ")
+      cardic['cartype'] = cartype
+    except KeyError:
+      print("something went wrong entering the key into the dic.", traceback.print_exc())
+    except ValueError:
+      print('something went wrong', traceback.print_exc())
+    else:
+      self.cars.append(cardic)
+      print(self.cars[0]['cartype'])
