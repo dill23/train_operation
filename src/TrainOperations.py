fd4281b2ec367968ebce941eef26bbd09be53763
dill23 <66688804+dill23@users.noreply.github.com>
2023-11-08 22:23:52 +0000

    tested the delete function

--- src/TrainOperations.py
+++ src/TrainOperations.py
@@ -1,18 +1,18 @@
  
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __author__ = "Dylan Hilse"
 import train_info as ti
-import network.network
-import network.client
+#import network.network
+#import network.client
 
 def main():
-  #train = ti.TrainInfo()
+  train = ti.TrainInfo()
   #train.maketrain()
   #train.addcar()
-  #train.deleteTrain()
-  #train.saveinfo()
-  network.start()
+  train.deleteTrain()
+  train.saveinfo()
+  #network.start()
  
 if __name__ == '__main__':
     main()
