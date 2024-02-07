773f20ef1f3610c80b98f37bd436df5fe3e54090
dill23 <66688804+dill23@users.noreply.github.com>
2024-02-07 21:10:37 +0000

    encryption

--- src/security.py
+++ src/security.py
@@ -1,3 +1,13 @@
 from  cryptography.fernet import Fernet
 
-class security()
+class security():
+  
+    def __init__(self):
+        self.key = Fernet.generate_key()
+        self.fernet = Fernet(self.key)
+
+    def encrypt(self, message):
+        return self.fernet.encrypt(message.encode())
+
+    def decrypt(self, message):
+        return self.fernet.decrypt(message).decode()
